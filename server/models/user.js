fd5b0338b25d65911c69217c08cf54b76559b968
hamza658 <66943287+hamza658@users.noreply.github.com>
2023-03-12 18:41:48 +0100

    commit

---
+++ server/models/user.js
@@ -0,0 +1,40 @@
+import mongoose, { Schema } from"mongoose";
+
+const userSchema=new Schema(
+  {
+    userName:{
+      type:String,
+      require:true
+    },
+    email:{
+      type:String,
+      require:true
+    },
+    password:{
+      type:String
+    },
+    token:{
+      type:String
+    },
+    ProfilePhoto:{
+      type:String
+    },
+    resetTokens:{
+      type:String,
+      default:""
+    },
+  
+    OTPReset: {
+      type: String,
+      default:null
+    }
+    ,
+    Verified: {
+      type: Boolean,
+      default: true,
+    },
+  }
+);
+const User=mongoose.model("user",userSchema);
+export{User};
+
